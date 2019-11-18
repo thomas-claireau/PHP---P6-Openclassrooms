01ec783143d17a5c21ce21dcc13081516ed0e91c
Thomas Claireau <thomas.claireau@gmail.com>
2019-11-18 19:34:07 +0100

    enable tracy debugger

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 /* Get the necessary elements by using namespaces (uncomment Tracy to get the Debugger) */
 use App\Router;
-// use Tracy\Debugger;
+use Tracy\Debugger;
 
 /* Required call to load the classes with the Composer Autoload */
 require_once '../vendor/autoload.php';
@@ -11,7 +11,7 @@ require_once '../vendor/autoload.php';
 $router = new Router();
 
 /* Test Zone (uncomment Debugger to get Tracy & var_dump to get router object) */
-// Debugger::enable();
+Debugger::enable();
 // var_dump($router);
 
 /* Run application */
