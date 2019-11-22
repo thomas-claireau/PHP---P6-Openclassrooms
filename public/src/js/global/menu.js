95c20ec4f4d5fe9f28076af2990a56a749af7c83
Thomas Claireau <thomas.claireau@gmail.com>
2019-11-22 19:36:20 +0100

    add class has scrolled on menu

---
+++ public/src/js/global/menu.js
@@ -0,0 +1,10 @@
+window.addEventListener("DOMContentLoaded", event => {
+  const menu = document.querySelector("#menu");
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 30) {
+      menu.classList.add("has-scrolled");
+    } else {
+      menu.classList.remove("has-scrolled");
+    }
+  });
+});
--- public/src/js/main.js
+++ public/src/js/main.js
@@ -1,5 +1,7 @@
 import "../scss/main.scss";
 
+import "./global/menu";
+
 window.addEventListener("DOMContentLoaded", event => {
   console.log("passe");
   console.log('"pas');
