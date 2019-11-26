76381078987eedbc6a9ac7aef65114d005674f00
Thomas Claireau <thomas.claireau@gmail.com>
2019-11-26 19:58:34 +0100

    front contact page -> desktop view OK

--- public/src/js/components/formulaire.js
+++ public/src/js/components/formulaire.js
@@ -2,12 +2,15 @@ window.addEventListener("DOMContentLoaded", event => {
   const formulaire = document.querySelector(".formulaire");
 
   if (formulaire) {
-    const inputs = formulaire.querySelectorAll("input");
-    console.log(inputs);
+    const inputs = formulaire.querySelectorAll(".input");
 
     inputs.forEach(input => {
-      input.addEventListener("keypress", e => {
-        // code here
+      input.addEventListener("input", e => {
+        if (input.value.length > 0) {
+          input.classList.add("active");
+        } else {
+          input.classList.remove("active");
+        }
       });
     });
   }
