a40afd89ce698d96c812496a46fd8a07fb7a1302
Thomas Claireau <thomas.claireau@gmail.com>
2019-11-30 20:48:25 +0100

    add function check input

--- public/src/js/functions.js
+++ public/src/js/functions.js
@@ -14,5 +14,26 @@ export default {
         svg.classList.add("activeSvg");
       });
     });
+  },
+  checkInput: (input, regex, match) => {
+    const parentInput = input.parentNode.parentNode;
+    const condition = match
+      ? input.value.match(regex)
+      : !input.value.match(regex);
+
+    if (input.value.length > 0) {
+      input.classList.add("active");
+      if (condition) {
+        parentInput.classList.remove("error");
+        parentInput.classList.add("ok");
+      } else {
+        parentInput.classList.remove("ok");
+        parentInput.classList.add("error");
+      }
+    } else {
+      input.classList.remove("active");
+      parentInput.classList.remove("error");
+      parentInput.classList.remove("ok");
+    }
   }
 };
