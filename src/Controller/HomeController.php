8657bb9901195620940c7e34fe5885412422dca1
Thomas Claireau <thomas.claireau@gmail.com>
2019-12-01 14:25:40 +0100

    home -> fix backend -> remove useless code

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Model\Factory\ModelFactory;
 use Twig\Error\LoaderError;
 use Twig\Error\RuntimeError;
 use Twig\Error\SyntaxError;
@@ -23,15 +22,8 @@ class HomeController extends MainController
      */
     public function defaultMethod()
     {
-        $allUsers = $this->getAllUsers();
-
         return $this->render('home.twig', [
-            'allUsers' => $this->getAllUsers(),
-        ]);
-    }
 
-    public function getAllUsers()
-    {
-        return ModelFactory::getModel('User')->listData();
+        ]);
     }
 }
