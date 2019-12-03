8ee41356763da8524bde5f4cd0e9a30bde31ec7f
Thomas Claireau <thomas.claireau@gmail.com>
2019-12-03 08:51:39 +0100

    admin comment in progress

--- public/src/js/pages/admin.js
+++ public/src/js/pages/admin.js
@@ -1,8 +1,32 @@
 window.addEventListener("DOMContentLoaded", event => {
   const requestUri = location.pathname + location.search;
-  console.log(requestUri);
   const linkTarget = document.querySelector(
     `.admin .sidebar a[href="${requestUri}"]`
   );
   linkTarget.classList.add("active");
+
+  // account view
+  const isAccountView = document.querySelector(".admin .account.view");
+
+  if (isAccountView) {
+    const form = isAccountView.querySelector("form");
+    const containerAction = isAccountView.querySelector(".actions");
+
+    if (containerAction) {
+      const trigerForm = containerAction.querySelector(".triggerForm");
+
+      trigerForm.addEventListener("click", () => {
+        if (form.classList.contains("disabled")) {
+          trigerForm.textContent = "Annuler";
+          trigerForm.classList.remove("vertFonce");
+          trigerForm.classList.add("rougeFonce");
+        } else {
+          trigerForm.textContent = "Mettre à jour";
+          trigerForm.classList.remove("rougeFonce");
+          trigerForm.classList.add("vertFonce");
+        }
+        form.classList.toggle("disabled");
+      });
+    }
+  }
 });
