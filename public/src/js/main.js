94386a9a6e7bdb5a7560f78d270129856577d645
Thomas Claireau <thomas.claireau@gmail.com>
2019-12-10 08:14:55 +0100

    admin -> sidebar scrolled OK

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -1,58 +1,58 @@
-import "../scss/main.scss";
+import '../scss/main.scss';
 
-import "./global/menu";
-import functions from "./functions";
+import './global/menu';
+import functions from './functions';
 
-import "./components/formulaire";
+import './components/formulaire';
 
-import "./pages/admin";
+import './pages/admin';
 
-window.addEventListener("DOMContentLoaded", event => {
-  functions.injectSvg();
+window.addEventListener('DOMContentLoaded', (event) => {
+	functions.injectSvg();
 
-  // smooth-scrool sur les ancres (vers #target)
-  const pageContainers = $("html, body");
-  const headerH = $("header").outerHeight();
+	// smooth-scrool sur les ancres (vers #target)
+	const pageContainers = $('html, body');
+	const headerH = $('header').outerHeight();
 
-  $(".js-smooth-scroll").click(function(e) {
-    e.preventDefault();
-    const target = $(this).attr("href");
-    if (!$(target).length) return;
-    const offset = parseInt($(this).data("offset")) || headerH;
-    pageContainers.animate(
-      {
-        scrollTop: $(target).offset().top - 50
-      },
-      400
-    );
-  });
+	$('.js-smooth-scroll').click(function(e) {
+		e.preventDefault();
+		const target = $(this).attr('href');
+		if (!$(target).length) return;
+		const offset = parseInt($(this).data('offset')) || headerH;
+		pageContainers.animate(
+			{
+				scrollTop: $(target).offset().top - 50,
+			},
+			400
+		);
+	});
 
-  // get url back
-  const urlReferrer = document.referrer;
-  const urlHome = window.location.origin;
-  const btnBack = document.querySelector("a.back");
+	// get url back
+	const urlReferrer = document.referrer;
+	const urlHome = window.location.origin;
+	const btnBack = document.querySelector('a.back');
 
-  if (btnBack) {
-    if (urlReferrer) {
-      btnBack.href = urlReferrer;
-    } else {
-      btnBack.href = urlHome;
-    }
-  }
+	if (btnBack) {
+		if (urlReferrer) {
+			btnBack.href = urlReferrer;
+		} else {
+			btnBack.href = urlHome;
+		}
+	}
 });
 
 // footer toujours en bas (sauf page formulaire)
 document.onreadystatechange = function() {
-  if (document.readyState == "complete") {
-    const pageContact = document.querySelector("body.contact");
-    const pagesLog = document.querySelector("#log");
-    const footer = document.querySelector("footer");
+	if (document.readyState == 'complete') {
+		const pageContact = document.querySelector('body.contact');
+		const pagesLog = document.querySelector('#log');
+		const footer = document.querySelector('footer');
 
-    if (footer && !pageContact && !pagesLog) {
-      const heightFooter = Number(footer.getBoundingClientRect().height);
+		if (footer && !pageContact && !pagesLog) {
+			const heightFooter = Number(footer.getBoundingClientRect().height);
 
-      const contentPage = document.querySelector("main");
-      contentPage.style.minHeight = "calc(100vh - " + heightFooter + "px)";
-    }
-  }
+			const contentPage = document.querySelector('main');
+			contentPage.style.minHeight = 'calc(100vh - ' + heightFooter + 'px)';
+		}
+	}
 };
--- public/src/js/pages/admin.js
+++ public/src/js/pages/admin.js
@@ -9,10 +9,31 @@ require.context(
 	/.*/
 );
 
-window.addEventListener('DOMContentLoaded', (event) => {
+// sidebar scroll
+// const sidebar = document.querySelector('.admin .sidebar');
+// document.onreadystatechange = function() {
+// 	console.log(document.readyState);
+// 	if (document.readyState == 'interactive') {
+// 		console.log(window.innerHeight);
+// 	}
+// };
+
+document.addEventListener('DOMContentLoaded', (event) => {
 	const requestUri = location.pathname + location.search;
 	const linkTarget = document.querySelector(`.admin .sidebar a[href="${requestUri}"]`);
 	if (linkTarget) {
 		linkTarget.classList.add('active');
 	}
+
+	document.onreadystatechange = function() {
+		if (document.readyState == 'complete') {
+			// scroll sidebar
+			const sidebar = document.querySelector('.admin .sidebar ul');
+			const windowHeight = window.innerHeight;
+
+			if (sidebar.scrollHeight > windowHeight - 100) {
+				sidebar.parentNode.classList.add('has-scrolled');
+			}
+		}
+	};
 });
