8284395fe8b68edd6a5cba6678222ea422730614
Thomas Claireau <thomas.claireau@gmail.com>
2019-12-27 10:17:42 +0100

    mobile menu OK

--- public/src/js/global/menu.js
+++ public/src/js/global/menu.js
@@ -1,12 +1,36 @@
 window.addEventListener("DOMContentLoaded", event => {
   const menu = document.querySelector("#menu");
+  const barsMenuMobile = document.querySelector('.bars-menu-mobile');
 
   if (menu) {
+    if (barsMenuMobile) {
+      const events = ['click', 'touch'];
+
+      events.forEach(event => {
+        barsMenuMobile.addEventListener(event, () => {
+          document.querySelector('body').classList.toggle('menu-burger-open');
+
+          const clickableItems = menu.querySelectorAll('a');
+
+          if (clickableItems) {
+            clickableItems.forEach(item => {
+              events.forEach(event => {
+                item.addEventListener(event, () => {
+                  document.querySelector('body').classList.remove('menu-burger-open');
+                })
+              })
+            })
+          }
+        })
+      })
+    }
     window.addEventListener("scroll", () => {
       if (window.scrollY > 30) {
         menu.classList.add("has-scrolled");
+        barsMenuMobile.classList.add("has-scrolled");
       } else {
         menu.classList.remove("has-scrolled");
+        barsMenuMobile.classList.remove("has-scrolled");
       }
     });
   }
