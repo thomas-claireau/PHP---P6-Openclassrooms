f9ceb253ca875ecccc2352dd19ee80cd709f9500
Thomas Claireau <thomas.claireau@gmail.com>
2020-01-08 07:54:56 +0100

    update db.sql

--- config/db.sql
+++ config/db.sql
@@ -1,85 +1,53 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+DROP DATABASE IF EXISTS blog_thomas_claireau;
+CREATE DATABASE blog_thomas_claireau CHARACTER SET 'utf8';
+
+USE blog_thomas_claireau;
 
 -- --------------------------------------------------------
-
---
--- Structure de la table `Comment`
---
-
-CREATE TABLE `Comment` (
-  `id` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL,
-  `id_post` int(11) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `content` text NOT NULL,
-  `date` datetime NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Structure de la table `Post`
---
-
-CREATE TABLE `Post` (
-  `id` int(110) NOT NULL,
-  `id_user` int(11) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `date` datetime NOT NULL,
-  `description` text NOT NULL,
-  `content` text NOT NULL,
-  `main_img_path` varchar(255) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Structure de la table `User`
 --
 
 CREATE TABLE `User` (
-  `id` int(11) NOT NULL,
-  `prenom` varchar(255) NOT NULL,
-  `nom` varchar(255) NOT NULL,
-  `mail` varchar(255) NOT NULL,
-  `password` varchar(1000) NOT NULL,
-  `admin` tinyint(1) NOT NULL,
-  `actif` tinyint(1) NOT NULL,
-  `avatar_img_path` varchar(1000) DEFAULT NULL,
-  `token` varchar(255) DEFAULT NULL,
+  `id` SMALLINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `prenom` VARCHAR(255) NOT NULL,
+  `nom` VARCHAR(255) NOT NULL,
+  `mail` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(1000) NOT NULL,
+  `admin` TINYINT(1) NOT NULL,
+  `actif` TINYINT(1) NOT NULL,
+  `avatar_img_path` VARCHAR(1000) DEFAULT NULL,
+  `token` VARCHAR(255) DEFAULT NULL,
   `dateToken` datetime NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
 
 --
--- Index pour la table `Comment`
+-- Structure de la table `Post`
 --
-ALTER TABLE `Comment`
-  ADD PRIMARY KEY (`id`);
+
+CREATE TABLE `Post` (
+  `id` SMALLINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `id_user` SMALLINT NOT NULL,
+  `title` VARCHAR(255) NOT NULL,
+  `date` DATETIME NOT NULL,
+  `description` TEXT NOT NULL,
+  `content` TEXT NOT NULL,
+  `main_img_path` VARCHAR(255) DEFAULT NULL,
+  CONSTRAINT fk_post_id_user FOREIGN KEY (`id_user`) REFERENCES User(`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
 
 --
--- Index pour la table `Post`
+-- Structure de la table `Comment`
 --
-ALTER TABLE `Post`
-  ADD PRIMARY KEY (`id`);
 
---
--- Index pour la table `User`
---
-ALTER TABLE `User`
-  ADD PRIMARY KEY (`id`);
-
---
-ALTER TABLE `Comment`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-
---
--- AUTO_INCREMENT pour la table `Post`
---
-ALTER TABLE `Post`
-  MODIFY `id` int(110) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
-
---
--- AUTO_INCREMENT pour la table `User`
---
-ALTER TABLE `User`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-COMMIT;
+CREATE TABLE `Comment` (
+  `id` SMALLINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `id_user` SMALLINT NOT NULL,
+  `id_post` SMALLINT NOT NULL,
+  `title` VARCHAR(255) NOT NULL,
+  `content` TEXT NOT NULL,
+  `date` DATETIME NOT NULL,
+  CONSTRAINT fk_comment_id_user FOREIGN KEY (`id_user`) REFERENCES User(`id`),
+  CONSTRAINT fk_comment_id_post FOREIGN KEY (`id_post`) REFERENCES Post(`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
